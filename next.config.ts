274e88204cb9ab150b597472f4b11d0b58c00f28
Daniel Apodaca <apodacaduron@gmail.com>
2025-09-22 16:08:02 -0600

    update config to set header

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your app.
+        source: '/(rsvp|countdown)/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
