b5b8915aedd79e105bc689ff28d64c8a817c5733
Daniel Apodaca <apodacaduron@gmail.com>
2025-09-22 17:49:03 -0600

    update headers

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   async headers() {
     return [
       {
-        // Apply these headers to all routes in your app.
-        source: '/(rsvp|countdown)/:path*',
+        source: "/rsvp/:path*",
         headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'ALLOWALL',
-          },
-          {
-            key: "Content-Security-Policy",
-            value: "frame-ancestors *",
-          },
+          { key: "X-Frame-Options", value: "ALLOWALL" },
+          { key: "Content-Security-Policy", value: "frame-ancestors *" },
         ],
       },
-    ];
+      {
+        source: "/countdown/:path*",
+        headers: [
+          { key: "X-Frame-Options", value: "ALLOWALL" },
+          { key: "Content-Security-Policy", value: "frame-ancestors *" },
+        ],
+      },
+    ]
   },
-};
+}
 
 export default nextConfig;
