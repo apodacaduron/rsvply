990be5e8071b4a3bb8bcda1c96d2bfeda4166884
Daniel Apodaca <apodacaduron@gmail.com>
2025-09-22 18:44:26 -0600

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,35 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-}
+  async headers() {
+    return [
+      {
+        source: "/(rsvp|countdown)/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors *",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://canva-embed.com",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
